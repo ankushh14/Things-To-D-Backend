333629ff8e7cbcd5ee6fa8229030278b39749d04
Ankush <ankushshenoy97@gmail.com>
2023-09-01 00:03:44 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+mongoose.connect(`mongodb+srv://ankushshenoy123:${process.env.PASSWORD}@things-to-d-db.iyrerfs.mongodb.net/Things-To-D-Db?retryWrites=true&w=majority`);
+app.use("/auth",userRouter);
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("Server Started");
+})
---
+++ routes/userRouter.js
@@ -0,0 +1,47 @@
+import express from "express"
+import bcrypt from "bcrypt";
+import { userModel } from "../MongoModels/userModel.js";
+import jwt from "jsonwebtoken";
+
+const userRouter = express.Router();
+
+userRouter.post("/register",async(req,res)=>{
+    const {username,password} = req.body;
+    const user = await userModel.findOne({username});
+    if(user){
+        return res.json({
+            message:"User Already Exists!"
+        })
+    }
+    const hashPassword = await bcrypt.hash(password,10);
+    const newUser = new userModel({username,password:hashPassword});
+    await newUser.save();
+    res.json({
+        message:"User Succesfully Registered!!"
+    })
+})
+
+userRouter.post("/login",async(req,res)=>{
+    const {username,password} = req.body;
+    const user = await userModel.findOne({username});
+    if(!user){
+        return res.json({
+            message:"User Not Found!!"
+        })
+    }
+    const intermediate = await bcrypt.compare(password,user.password);
+    if(intermediate){
+        const token = jwt.sign({username:user.username},process.env.SECRET);
+        res.json({
+            userID:user._id,
+            token,
+            message:"Login Succesfull!",
+        })
+    }else{
+        return res.json({
+            message:"Invalid Credentials!"
+        })
+    }
+})
+
+export default userRouter;
